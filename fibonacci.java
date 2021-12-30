daa0f5d16742b05d12fea012b7838610771ced49
Prithvijit Basak <68730583+prithvijitbasak@users.noreply.github.com>
2021-12-30 21:12:45 +0530

    Create fibonacci.java

---
+++ fibonacci.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class fibonacci
+{
+	public static void main(String args[])
+	{
+		System.out.println("Enter the range");
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int dp[]=new int[n+1];
+		dp[0]=0;
+		dp[1]=1;
+		for(int i=2;i<n;i++)
+		{
+			dp[i]=dp[i-1]+dp[i-2];// fib(n)=fib(n-1)+fib(n-2);//Bottom up approach(tabulation)
+		}
+		System.out.println("The fibonacci numbers are:-");
+		for(int i=0;i<n;i++)
+			System.out.print(dp[i]+" ");
+		System.out.println("");
+	}
+}
